3e7b983c1485fea1530e96d6cb47df0b27ef45f8
Alvarden <alvaroracchumi@hotmail.com>
2019-05-08 13:17:20 -0500

    Conexión temporal (hasta ver como arreglarlo)

--- Startup.cs
+++ Startup.cs
@@ -36,8 +36,8 @@ namespace LKBHistorial
            
 
             services.AddMvc();
-            services.AddDbContext<MvcContext>(options=>
-                            options.UseSqlServer(Configuration.GetConnectionStrings("Default")));
+            services.AddDbContext<MvcContext>(options=>options.UseSqlServer("Server=tcp:lkb.database.windows.net,1433;Initial Catalog=LKBData;Persist Security Info=False;User ID=lkbadmin;Password=LKBHistorial!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+                            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
